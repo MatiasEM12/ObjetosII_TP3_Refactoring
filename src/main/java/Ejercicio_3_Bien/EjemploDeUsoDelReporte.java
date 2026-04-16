09b12da62fb43a96950ad9bf27c67d4d886c7189
MatiasEM12 <matiasemimellado@gmail.com>
2026-04-16 20:34:07 -0300

    rename

--- src/main/java/Ejercicio_3_Bien/EjemploDeUsoDelReporte.java
+++ src/main/java/Ejercicio_3_Bien/EjemploDeUsoDelReporte.java
@@ -12,8 +12,7 @@ public class EjemploDeUsoDelReporte {
         var g1 = new GastoDesayuno(5000);
         var g2 = new GastoCena(1500);
         var g3= new GastoAlquilerDeAuto(10000);
-        //g1.tipoGasto = TipoDeGasto.DESAYUNO;
-        //g1.monto =1000;
+
         var reporte = new ReporteDeGastos();
         ArrayList<String> reporteGenerado= reporte.generarReporte(List.of(g1,g2,g3));
         for(String st : reporteGenerado){
